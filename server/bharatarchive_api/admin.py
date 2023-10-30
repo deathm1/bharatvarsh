f405c87dd10e7eb3bef259a43e08ff64a6ab7526
harsh <harsh@asasd.com>
2023-10-30 20:08:25 +0530

    loaded sa data

--- server/bharatarchive_api/admin.py
+++ server/bharatarchive_api/admin.py
@@ -25,14 +25,9 @@ class language_admin(admin.ModelAdmin):
     list_display = [
         "language_name",
         "language_code_alpha2",
-        "language_code_alpha3_t",
-        "language_code_alpha3_b",
         "language_display_name",
         "is_active",
-        "created_date",
-        "created_by",
-        "updated_date",
-        "updated_by",
+        "created_date"
     ]
 
 
@@ -91,3 +86,36 @@ class verses_of_chapters_admin(admin.ModelAdmin):
 
 
 admin.site.register(verses_of_chapters, verses_of_chapters_admin)
+
+
+class cms_views_admin(admin.ModelAdmin):
+    list_display = [
+        "website_name",
+        "footer_text",
+        "change_theme_tooltip",
+        "menu_tooltip",
+        "logo_tooltip",
+        "created_date",
+        "created_by",
+        "updated_date",
+        "updated_by",
+    ]
+
+
+admin.site.register(cms_views, cms_views_admin)
+
+
+class cms_tabs_admin(admin.ModelAdmin):
+    list_display = [
+        "tab_name",
+        "tab_dsiplay_name",
+        "tab_endpoint",
+        "is_active",
+        "created_date",
+        "created_by",
+        "updated_date",
+        "updated_by",
+    ]
+
+
+admin.site.register(cms_tabs, cms_tabs_admin)
--- server/bharatarchive_api/models.py
+++ server/bharatarchive_api/models.py
@@ -20,12 +20,12 @@ class language(models.Model):
     class Meta:
         verbose_name_plural = "Available Languages"
 
-    language_name = models.CharField(max_length=100)
-    language_display_name = models.CharField(max_length=100)
+    language_name = models.CharField(max_length=100, null=False)
+    language_display_name = models.CharField(max_length=100, null=False)
 
-    language_code_alpha2 = models.CharField(max_length=10, default="N/A")
-    language_code_alpha3_t = models.CharField(max_length=10, default="N/A")
-    language_code_alpha3_b = models.CharField(max_length=10, default="N/A")
+    language_code_alpha2 = models.CharField(max_length=10, default="N/A", null=True)
+    language_code_alpha3_t = models.CharField(max_length=10, default="N/A", null=True)
+    language_code_alpha3_b = models.CharField(max_length=10, default="N/A", null=True)
 
     is_active = models.BooleanField(default=1)
     created_by = models.CharField(max_length=100, null=False)
@@ -80,3 +80,28 @@ class verses_of_chapters(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_by = models.CharField(max_length=100, null=False)
     updated_date = models.DateTimeField(auto_now=True)
+
+class cms_views(models.Model):
+    class Meta:
+        verbose_name_plural = "Content Management System Website Views"
+    website_name = models.CharField(max_length=25, null=False)
+    footer_text = models.CharField(max_length=200, null=False)
+    change_theme_tooltip = models.CharField(max_length=50, null=False)
+    menu_tooltip = models.CharField(max_length=50, null=False)
+    logo_tooltip = models.CharField(max_length=50, null=False)
+    created_by = models.CharField(max_length=100, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_by = models.CharField(max_length=100, null=False)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class cms_tabs(models.Model):
+    class Meta:
+        verbose_name_plural = "Content Management System Website Tabs"
+    tab_name = models.CharField(max_length=25, null=False)
+    tab_dsiplay_name = models.CharField(max_length=25, null=False)
+    tab_endpoint = models.CharField(max_length=200, null=False)
+    is_active = models.BooleanField(default=1)
+    created_by = models.CharField(max_length=100, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_by = models.CharField(max_length=100, null=False)
+    updated_date = models.DateTimeField(auto_now=True) 
